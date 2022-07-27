7e9a2804e56521950104aab9df2ee4ba9c241a3b
Vitaliy Zhyryk <80642298+WindyBoss@users.noreply.github.com>
2022-07-27 10:21:52 +0200

    added error boundary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NightlightTwoToneIcon from '@mui/icons-material/NightlightTwoTone';
 import LightModeTwoToneIcon from '@mui/icons-material/LightModeTwoTone';
 import { IconButton } from '@mui/material';
+import { withErrorBoundary, useErrorBoundary } from 'react-use-error-boundary';
 
 import { ThemeSwitcher } from './AppLayoutStyles.jsx';
 
@@ -31,7 +32,7 @@ import { LinearProgressStyled } from './CommonComponents';
 * reactLazyLoad - custom function, which helps to split the code into multiple components, 
 which are downloaded not during the first open of page, but when they are necessary
 All export must be default
-*/ 
+*/
 const CounterPage = reactLazyLoad('Counter');
 const SignupForm = reactLazyLoad('SignupForm');
 const ColorPicker = reactLazyLoad('ColorPicker');
@@ -51,10 +52,14 @@ const PublicationList = reactLazyLoadReaderApi('PublicationList');
 const CreatePublication = reactLazyLoadReaderApi('CreatePublication');
 const Preview = reactLazyLoadReaderApi('Preview');
 
-export default function App() {
+function App() {
   const { mainTheme, changeTheme } = useContext(themeContext);
   // The next state helps to measure the speed of the internet and set as variable
   const [internetSpeed, setInternetSpeed] = useState(0);
+  const [error, resetError] = useErrorBoundary(
+    // You can optionally log the error to an error reporting service
+    (error, errorInfo) => console.log(error, errorInfo)
+  );
 
   return (
     <div>
@@ -82,55 +87,79 @@ export default function App() {
           callbackFunctionOnNetworkTest={speed => setInternetSpeed(speed)}
         />
         {/* The component with lazy loading must the replaced before they are loaded to some component or JS will show mistake, so Suspense is used */}
-        <Suspense
-          fallback={
-            internetSpeed > 100 ? (
-              ''
-            ) : (
-              <LinearProgressStyled
-                colors={mainTheme.colors}
-                addFeat={{
-                  minWidth: '80%',
-                  marginLeft: 'auto',
-                  marginRight: 'auto',
-                  height: ' 30px',
-                  borderRadius: '5px',
-                }}
-              />
-            )
-          }
-        >
-          <Routes>
-            <Route
-              path="/react-homework-template/"
-              element={<Appbar />}
-            >
-              <Route index element={<HomePage />} />
-              <Route path="Counter" element={<CounterPage />} />
-              <Route path="SignupForm" element={<SignupForm />} />
-              <Route path="ColorPicker" element={<ColorPicker />} />
-              <Route path="Clock" element={<Clock />} />
-              <Route path="News" element={<News />} />
-              <Route path="Video" element={<Video videos={videos} />} />
-              <Route
-                path="Reader"
-                element={<Reader publications={publications} />}
-              />
-              <Route path="ReaderWithApi" element={<ReaderAppbar />}>
+        {error ? (
+          <div>
+            <p>{error.message}</p>
+            <button onClick={resetError}>Try again</button>
+          </div>
+        ) : (
+          <Suspense
+            fallback={
+              internetSpeed > 100 ? (
+                ''
+              ) : (
+                <LinearProgressStyled
+                  colors={mainTheme.colors}
+                  addFeat={{
+                    minWidth: '80%',
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                    height: ' 30px',
+                    borderRadius: '5px',
+                  }}
+                />
+              )
+            }
+          >
+            <Routes>
+              <Route path="/react-homework-template/" element={<Appbar />}>
+                <Route index element={<HomePage />} />
+                <Route path="Counter" element={<CounterPage />} />
+                <Route path="SignupForm" element={<SignupForm />} />
+                <Route path="ColorPicker" element={<ColorPicker />} />
+                <Route path="Clock" element={<Clock />} />
+                <Route path="News" element={<News />} />
+                <Route path="Video" element={<Video videos={videos} />} />
                 <Route
-                  index
+                  path="Reader"
+                  element={<Reader publications={publications} />}
+                />
+                <Route path="ReaderWithApi" element={<ReaderAppbar />}>
+                  <Route
+                    index
+                    element={
+                      <RedirectContainer containerText="Choose Preview Button" />
+                    }
+                  />
+
+                  <Route path="preview" element={<Preview />}>
+                    <Route path=":publicationId" element={<ListPage />} />
+                  </Route>
+                  <Route path="list" element={<PublicationList />}>
+                    <Route path=":publicationId" element={<ListPage />} />
+                  </Route>
+                  <Route path="create" element={<CreatePublication />} />
+                  <Route
+                    path="*"
+                    element={
+                      <RedirectContainer containerText="Sorry There is no such way" />
+                    }
+                  />
+                </Route>
+                <Route path="Tabs" element={<Tabs items={tabs} />} />
+                <Route path="TodoList" element={<TodoList todos={todos} />} />
+                <Route path="Pokemon" element={<Pokemon />} />
+                <Route
+                  path="SkipEffectOnFirstRender"
+                  element={<SkipEffectOnFirstRender />}
+                />
+                <Route path="Friends" element={<Friends />} />
+                <Route
+                  path="MediaPlayer"
                   element={
-                    <RedirectContainer containerText="Choose Preview Button" />
+                    <MediaPlayer source="http://media.w3.org/2010/05/sintel/trailer.mp4" />
                   }
                 />
-
-                <Route path="preview" element={<Preview />}>
-                  <Route path=":publicationId" element={<ListPage />} />
-                </Route>
-                <Route path="list" element={<PublicationList />}>
-                  <Route path=":publicationId" element={<ListPage />} />
-                </Route>
-                <Route path="create" element={<CreatePublication />} />
                 <Route
                   path="*"
                   element={
@@ -138,30 +167,12 @@ export default function App() {
                   }
                 />
               </Route>
-              <Route path="Tabs" element={<Tabs items={tabs} />} />
-              <Route path="TodoList" element={<TodoList todos={todos} />} />
-              <Route path="Pokemon" element={<Pokemon />} />
-              <Route
-                path="SkipEffectOnFirstRender"
-                element={<SkipEffectOnFirstRender />}
-              />
-              <Route path="Friends" element={<Friends />} />
-              <Route
-                path="MediaPlayer"
-                element={
-                  <MediaPlayer source="http://media.w3.org/2010/05/sintel/trailer.mp4" />
-                }
-              />
-              <Route
-                path="*"
-                element={
-                  <RedirectContainer containerText="Sorry There is no such way" />
-                }
-              />
-            </Route>
-          </Routes>
-        </Suspense>
+            </Routes>
+          </Suspense>
+        )}
       </BrowserRouter>
     </div>
   );
 }
+
+export default withErrorBoundary(App);
--- src/helpers/reactLazyLoad.js
+++ src/helpers/reactLazyLoad.js
@@ -3,10 +3,14 @@ import { lazy } from 'react';
   * react lazy - function-promise that is asynchronously called and it returns import() function
  */
 
+// export const reactLazyLoad = componentName => {
+//   return lazy(() => {
+//     return import(`../pages/${componentName}`);
+//   });
+// };
+
 export const reactLazyLoad = componentName => {
-  return lazy(() => {
-    return import(`../pages/${componentName}`);
-  });
+  return lazy(() => new Promise(import(`../pages/${componentName}`))).then(() => throw new Error());
 };
 
 export const reactLazyLoadReaderApi = componentName => {
