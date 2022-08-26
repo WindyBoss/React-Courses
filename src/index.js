0685985960f24c4de9ee2d1da825721d8a7dde10
Vitaliy Zhyryk <80642298+WindyBoss@users.noreply.github.com>
2022-08-26 14:37:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,26 +10,18 @@ import { todoApi } from './service/apiService';
 
 import ThemeProvider from 'context/ThemeProvider';
 
-ReactDOM.render( <
-    React.StrictMode >
-    <
-    ApiProvider api = { todoApi } >
-    <
-    PersistGate loading = { 'loading...' }
-    persistor = { persistor } >
-    <
-    ThemeProvider >
-    <
-    Provider store = { store } >
-    <
-    App / >
-    <
-    /Provider>{' '} <
-    Toaster / >
-    <
-    /ThemeProvider>{' '} < /
-    PersistGate > { ' ' } <
-    /ApiProvider>{' '} < /
-    React.StrictMode > ,
-    document.getElementById('root')
+ReactDOM.render( 
+  <React.StrictMode>
+    <ApiProvider api={ todoApi }>
+    <PersistGate loading={ 'loading...' } persistor={ persistor }>
+      <ThemeProvider>
+        <Provider store={ store }>
+          <App/>
+          <Toaster/>
+         </Provider>   
+      </ThemeProvider>
+    </PersistGate> 
+    </ApiProvider> 
+  </React.StrictMode>,
+  document.getElementById('root')
 );
