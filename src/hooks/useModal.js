726bffec63e9e530483e3aedbbe1ed7180e242a8
Vitaliy Zhyryk <80642298+WindyBoss@users.noreply.github.com>
2022-08-26 13:39:30 +0200

    delete node modules

--- src/hooks/useModal.js
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-export const useModal = (isOpen = false) => {
-  const [showModal, setShowModal] = useState(isOpen);
-  const toggleModal = () => setShowModal(!showModal);
-
-  return [showModal, toggleModal];
-};
