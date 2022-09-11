cf931d66ff430efefe247448798555267d389bbd
Vitaliy Zhyryk <80642298+WindyBoss@users.noreply.github.com>
2022-09-11 12:31:13 +0200

    Add code

---
+++ src/index.js
@@ -0,0 +1,30 @@
+/** @format */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import contactApi from "./service/apiService";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApiProvider api={contactApi}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <App />
+            <Toaster />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </ApiProvider>
+  </React.StrictMode>,
+  document.querySelector("#root")
+);
